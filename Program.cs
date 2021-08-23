d5eb2edd64e8609bf8225aee285c47438f012db8
HarryD1447 <theharryd987@gmail.com>
2021-08-23 15:50:45 +0100

    This is the second commit

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace Git_Test
         {
             Console.WriteLine("This is the development branch.");
             Console.WriteLine("We use this branch to work on code that isn't live yet and hasn't been pushed to production");
+            Console.WriteLine("This is the first update commit to the github");
         }
     }
 }
