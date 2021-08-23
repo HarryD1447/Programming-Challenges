25a851c2845917ac15beeaf7c21893c8dafc44a4
HarryD1447 <theharryd987@gmail.com>
2021-08-23 20:20:53 +0100

    Binary search v1

--- C#/Binary Search/Program.cs
+++ C#/Binary Search/Program.cs
@@ -1,12 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Binary_Sort
 {
     class Program
     {
+
+        enum Result
+        {
+            Lower,
+            Higher, 
+            Equal
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the binary search challange solution.");
+            Console.WriteLine("Welcome to the binary search challange solution. (Number Wizard)");
+            Console.WriteLine("Think of a whole number that you want to guess from 1 to 1000 and i'll guess it!");
+            Console.WriteLine("Enter H,L or E to indicate if the guess was higher, lower or equal to the number");
+
+            bool found = false;
+            int guessCount = 0;
+
+            int top = 1000;
+            int bottom = 1;
+
+            while (!found)
+            {
+                int guess = Lerp(bottom, top);
+                Result response = GetUserResponse();
+
+                if (response == Result.Lower)
+                {
+                    top = guess;
+                    guessCount++;
+                }
+                else if (response == Result.Higher)
+                {
+                    bottom = guess;
+                    guessCount++;
+                }
+                else
+                {
+                    found = true;
+                    Console.WriteLine("See I managed to guess the number within {0} attempts", guessCount);
+                }
+            }
+        }
+
+        static int Lerp(int bottom, int top)
+        {
+            int distance = top - bottom;
+            int half = distance / 2;
+            return bottom + half;
+        }
+
+        static Result GetUserResponse()
+        {
+            Result r = Result.Equal;
+            bool ans = false;
+            while (!ans)
+            {
+                Console.WriteLine("Enter H,L or E");
+                string value = Console.ReadLine();
+                switch(value)
+                {
+                    case "H":
+                        r = Result.Higher;
+                        ans = true;
+                        break;
+                    case "L":
+                        r = Result.Lower;
+                        ans = true;
+                        break;
+                    case "E":
+                        r = Result.Equal;
+                        ans = true;
+                        break;
+                }
+            }
+            return r;
         }
     }
 }
