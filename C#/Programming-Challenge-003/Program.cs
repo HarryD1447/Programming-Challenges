bff7edb0e09de25c5a6cf1e5c88690e595d6767f
HarryD1447 <theharryd987@gmail.com>
2021-09-03 12:14:16 +0100

    File Created

---
+++ C#/Programming-Challenge-003/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Programming_Challenge_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static string GetString(string[] strings, int largestIndex)
+        {
+            int[] numberInts = new int[strings.Length];
+            for (int i = 0; i < strings.Length; i++)
+            {
+                numberInts[i] = int.Parse(strings[i]);
+            }
+
+            //Bubble sort
+            for (int x = 0; x < numberInts.Length; x++)
+            {
+                for (int y = 0; y < numberInts.Length - 1; y++)
+                {
+                    if (numberInts[y] > numberInts[y + 1])
+                    {
+                        //Flip
+                        int temp = numberInts[y];
+                        numberInts[y] = numberInts[y + 1];
+                        numberInts[y + 1] = temp;
+                    }
+                }
+            }
+
+            int value = numberInts[numberInts.Length - largestIndex];
+            return value.ToString();
+
+        }
+        
+    }
+}
---
+++ C#/Reverse-Integer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*Leetcode challange
+https://leetcode.com/problems/reverse-integer/ */
+
+namespace Reverse_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the 32 bit integer that you want reversing!");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("The reverse of {0} is {1}", number, ReverseInteger(number));
+        }
+
+        static int ReverseInteger(int num)
+        {
+            //Push & pop
+            int reversed = 0;
+            while (num > 0)
+            {
+                int remainder = num % 10;
+                if (reversed > 2147483650 / 10 || (reversed == 2147483650 / 10 && remainder > 7)) {reversed = 0; num = 0;}
+                if (reversed < 2147483650 / 10 || (reversed == 2147483650 / 10 && remainder < -8)) {reversed = 0; num = 0;}
+                reversed = reversed * 10 + remainder;
+                num /= 10;
+            }
+            return reversed;
+        }
+    }
+}
