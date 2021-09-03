567e0a71188211bbdf90830efb7f6836ef14fda1
HarryD1447 <theharryd987@gmail.com>
2021-09-03 14:17:54 +0100

    Remove_Element_Interview_Question-Complete

---
+++ C#/Remove-Element-Interview/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Remove_Element_Interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {2,2,2,2,2,2};
+            int k = RemoveElement(ref nums, 2);
+            Console.WriteLine("The new size of the array is {0}", k);
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+
+        public static int RemoveElement(ref int[] nums, int val)
+        {
+            int nextIndexPosition = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[nextIndexPosition] = nums[i];
+                    nextIndexPosition++;
+                }
+            }
+
+            return nextIndexPosition;
+        }
+    }
+}
