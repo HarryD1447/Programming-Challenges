1728d80fdae34aa4b151ee8f1dabb92f81855c59
HarryD1447 <theharryd987@gmail.com>
2021-09-07 14:14:33 +0100

    Reverse Link List Challenge Complete

---
+++ C#/Reverse-Link-List/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Reverse_Link_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+
+        static ListNode ReverseList(ListNode head) 
+        {
+            ListNode previousNode = null;
+
+            while (head != null)
+            {
+                ListNode next_node = head.next;
+                head.next = previousNode;
+                previousNode = head;
+                head = next_node;
+            }
+
+            return previousNode;
+        }
+    }
+
+    public class ListNode 
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int val=0, ListNode next=null) 
+      {
+         this.val = val;
+          this.next = next;
+      }
+  }
+}
