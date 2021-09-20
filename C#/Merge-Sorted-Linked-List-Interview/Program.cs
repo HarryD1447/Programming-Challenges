84adad6dab5c668afac2fffe8fee2f0f4d9eae1b
HarryD1447 <85346792+HarryD1447@users.noreply.github.com>
2021-09-20 16:45:35 +0100

    Reverse-Only-Letters-Commit

---
+++ C#/Merge-Sorted-Linked-List-Interview/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Merge_Sorted_Linked_List_Interview
+{
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Program
+    {
+
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            //Blank first node so return the linked list from the second position (index 1)
+            ListNode returnValue = new ListNode();
+            ListNode previousValue = returnValue;
+
+            while (l1 != null || l2 != null)
+            {
+                if (l1 == null)
+                {
+                    //Add l2
+                    previousValue.next = 
+                }
+                else if (l2 == null)
+                {
+                    //Add l1
+                }
+                else
+                {
+                    //Comparison
+                    if (l1.val < l2.val)
+                    {
+                        //Add l1
+                    }
+                    else
+                    {
+                        //Add l2
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#/Reverse-Only-Letters-Interview/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Reverse_Only_Letters_Interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseOnlyLetters("a-bC-dEf-ghIj"));
+        }
+
+        public static string ReverseOnlyLetters(string s)
+        {
+            char[] characters = s.ToCharArray();
+            if (characters.Length <= 1) { return s; }
+
+            int pointerA = 0;
+            int pointerB = characters.Length - 1;
+
+            while (pointerA != pointerB || pointerA < pointerB)
+            {
+                if (!isValidCharacter(characters[pointerA]))
+                {
+                    pointerA++;
+                    Console.WriteLine("Invalid character A");
+
+                }
+                else if (!isValidCharacter(characters[pointerB]))
+                {
+                    pointerB--;
+                    Console.WriteLine("Invalid character B");
+                }
+                else
+                {
+                    if (isValidCharacter(characters[pointerA]) && isValidCharacter(characters[pointerB]))
+                    {
+                        //Swap 
+                        char temp = characters[pointerB];
+                        characters[pointerB] = characters[pointerA];
+                        characters[pointerA] = temp;
+
+                        //Modify pointer
+                        pointerA++;
+                        pointerB--;
+
+                        Console.WriteLine("Swap {0}  ,   {1}",characters[pointerA],characters[pointerB]);
+                    }
+                }
+
+                //Check pointers
+                if (pointerA - 1 == pointerB){break;}
+
+            }
+
+            return new string(characters);
+        }
+
+        public static bool isValidCharacter(char c)
+        {
+            int value = (int)c;
+            if (c >= 97 && c <= 122)
+            {
+                return true;
+            }
+            else if (c >= 65 && c <= 90)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
